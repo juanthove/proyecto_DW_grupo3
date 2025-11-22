e3b8889a8a5b1ee7e378a33db6d1f1feb8180e90
Gabriela Sosa <gabriellasosa.f@gmail.com>
2025-11-21 21:29:39 -0300

    add API folder

---
+++ API/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cors());
+app.use(cors({origin:'*'}))
+
+//routes
+app.use('/products', require('./routes/products'));
+app.use('/cart', require('./routes/cart'));
+app.use('/users', require('./routes/users'));
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+});
